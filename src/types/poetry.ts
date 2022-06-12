4316556e4b15f380f07f45e74a5071468c50c566
forsilence <3312034934@qq.com>
2022-06-12 21:37:26 +0800

    module:add add statistic of poetry

--- src/types/poetry.ts
+++ src/types/poetry.ts
@@ -8,11 +8,27 @@ export type PoetryTang = {
   'no#': number
 }
 
+/**
+ * 单个诗词文件的统计：文件名和诗词数量
+ */
+export type FileMeta = {
+  name: string,
+  size: number
+}
+
+/**
+ * 文件统计格式: 统计一种类型的诗词的总数和每个文件的诗词的数量
+ */
+export type PoetryStatistic = {
+  total: number,
+  fileMetas: Array<FileMeta>
+}
+
 export type PoetryConfig = {
   tangUrl: string
 }
 
-export type ChinesePoetry = PoetryTang | string
+export type ChinesePoetry = PoetryTang | PoetryStatistic | string
 
 /**
  * the file loader to load the json file for specific structure
